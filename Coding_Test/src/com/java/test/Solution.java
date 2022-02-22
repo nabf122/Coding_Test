3dc6080c5b389b2502e283308a81b86b0c30cd0b
SQI <SQI@DESKTOP-J6JCNQH>
2022-02-22 16:11:50 +0900

    commit 2022-02-22 16:11

--- Coding_Test/src/com/java/test/Solution.java
+++ Coding_Test/src/com/java/test/Solution.java
@@ -3,37 +3,69 @@ package com.java.test;
 import java.util.Scanner;
 
 class Solution {
+	
+	static int[] numbers = new int[100];
+	static int[][] commands = new int[50][3];
+	
+	static int MAX_NUM = 0;
+	static int MAX_CMD = 0;
+	
+	Scanner scan = new Scanner(System.in);
+	
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = {};
         return answer;
     }
     
-    public static void main(String args[]) {
-    	int[] numbers = new int[100];
-    	int[][] commands = new int[3][50];
-    	
-    	Scanner scan = new Scanner(System.in);
-    	
-    	int i = 0;
+    public void insertNum() {
     	int s = 0;
-    	while(true){
-    		s = scan.nextInt();
-    		
-    		if(i > 100)
-    		{
-    			if(s != 101) {
-    				numbers[i] = s;
-            		i++;
-    			}else
-    				break;
-    		}else
-    			break;
-    	}
     	
-    	for(int j = 0; j < i+1; j++)
+    	System.out.print("입력 갯수 지정(MAX 100) : ");
+    	MAX_NUM = scan.nextInt();
+    	if(MAX_NUM <= 100 && MAX_NUM > 0) {
+    		for(int i = 0; i < MAX_NUM; i++) {
+    			System.out.print(i+1 + "번째 숫자 입력 : ");
+        		s = scan.nextInt();
+        		numbers[i] = s;
+            }
+        	
+        	for(int j = 0; j < MAX_NUM; j++)
+        	{
+        		System.out.print(numbers[j] + " ");
+        	}
+    	}else
+    		System.out.println("잘못된 입력 값");
+    }
+    
+    public void insertCmd() {
+    	System.out.print("입력 갯수 지정(MAX 50) : ");
+    	MAX_CMD = scan.nextInt();
+    	if(MAX_CMD <= 50 && MAX_CMD > 0) {
+    		for(int i = 0; i < MAX_CMD; i++) {
+    			for(int j = 0; j < 3; j++) {
+    				System.out.print("입력 : ");
+    				commands[i][j] = scan.nextInt();
+    			}
+            }
+    	}else
+    		System.out.println("잘못된 입력 값");
+    }
+    
+
+	public static void main(String args[]) {
+		
+		Solution sol = new Solution();
+
+		int[] result = new int[100];
+		
+    	sol.insertNum();
+    	sol.insertCmd();
+
+    	result = sol.solution(numbers, commands);
+    	
+    	for(int i = 0;i < MAX_NUM; i++)
     	{
-    		System.out.print(numbers[j] + " ");
+    		System.out.print(result[i] + " ");
     	}
-    	
     }
 }
